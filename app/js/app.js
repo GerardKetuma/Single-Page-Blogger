ea0a261563c308dd3b2f9a2e5c20b33e0670c3de
Gerard Ketuma <me@gerardketuma.com>
2015-05-12 00:44:37 -0500

    Add a basic setup for the single page blogger application

---
+++ app/js/app.js
@@ -0,0 +1,19 @@
+'use strict'
+
+angular.module(
+  'spBlogger',
+  [
+    'ui.router',
+    'spBlogger.posts',
+    'spBlogger.controllers',
+    'spBlogger.directives',
+    'spBlogger.filters',
+    'spBlogger.services'
+  ]
+);
+
+angular.module('spBlogger').value('version','V1.0');
+
+angular.module('spBlogger').run(['$state', function($state) {
+  $state.go('allPosts');
+}]);
